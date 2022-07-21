4bc2fa3590e36fa54fcb15d8342d8f284a2a7669
Nick Dexter <nickdexter23@gmail.com>
2022-07-20 21:08:12 -0500

    commit tree 732da27ebee3d5f9a19daab5bc949c8abe2f16af

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+    const messages = []
+  for (let i = 0; i < cards.length; i++) {
+    messages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+  }
+
+  return messages;
+}
+
+writeCards(cards);
+
+
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+  }
